fda901dd3649d39579a77bf950552c5b5a456183
JameyWoo <2622075127wjh@gmail.com>
2021-03-21 11:07:24 +0800

    发现在大量Put key的时候会出现索引值异常的情况, 如为很大的一个负值

--- test/goleveldb_test/db_test.go
+++
@@ -1,9 +0,0 @@
-/**
- * @Author: JameyWoo
- * @Email: 2622075127wjh@gmail.com
- * @Date: 2021/3/3
- * @Desc: goleveldb_test
- * @Copyright (c) 2021, JameyWoo. All rights reserved.
- */
-
-package goleveldb_test
